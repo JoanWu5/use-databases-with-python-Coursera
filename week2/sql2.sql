00da6efdc85349b80d38cb47c72858bf0e3112c0
joannwu <394104840@qq.com>
2019-05-27 10:13:57 +0800

    Initial commit

---
+++ week2/sql2.sql
@@ -0,0 +1,5 @@
+DELETE FROM Ages;
+INSERT INTO Ages (name, age) VALUES ('Leland', 35);
+INSERT INTO Ages (name, age) VALUES ('Saman', 32);
+INSERT INTO Ages (name, age) VALUES ('Nikolina', 30);
+INSERT INTO Ages (name, age) VALUES ('Kaitlan', 29);
